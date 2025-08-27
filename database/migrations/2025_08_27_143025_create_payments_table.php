5cf634b91fa3339e0f2958ccae5fbbb3c9799467
DexterX12 <65998342+DexterX12@users.noreply.github.com>
2025-08-27 10:24:38 -0500

    Refactor: Using user table instead of customers

--- database/migrations/2025_08_27_143025_create_payments_table.php
+++ database/migrations/2025_08_27_143025_create_payments_table.php
@@ -13,9 +13,9 @@ return new class extends Migration
     {
         Schema::create('payments', function (Blueprint $table) {
             $table->id();
-            $table->unsignedBigInteger('customer_id');
+            $table->unsignedBigInteger('user_id');
             $table->string('type');
-            $table->foreign('customer_id')->references('id')->on('customers');
+            $table->foreign('user_id')->references('id')->on('users');
             $table->timestamps();
         });
     }
--- database/migrations/2025_08_27_143032_create_orders_table.php
+++ database/migrations/2025_08_27_143032_create_orders_table.php
@@ -13,10 +13,10 @@ return new class extends Migration
     {
         Schema::create('orders', function (Blueprint $table) {
             $table->id();
-            $table->unsignedBigInteger('customer_id');
+            $table->unsignedBigInteger('users_id');
             $table->date('date');
             $table->string('description');
-            $table->foreign('customer_id')->references('id')->on('customers');
+            $table->foreign('users_id')->references('id')->on('users');
             $table->timestamps();
         });
     }
