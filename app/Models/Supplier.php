f13504e998969e6ad67b4f77f99c32b2593c1417
stufonn-cell <stufonn@gmail.com>
2025-09-11 11:37:18 -0500

    Add: Supplier controller

---
+++ app/Models/Supplier.php
@@ -0,0 +1,87 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Model;
+use Illuminate\Support\Facades\Date;
+
+class Supplier extends Model
+{
+    /**
+     * PRODUCT ATTRIBUTES
+     * $this->attributes['id'] - int - contains the supplier primary key (id)
+     * $this->attributes['name'] - string - contains the supplier name
+     * $this->attributes['email'] - string - contains the supplier email
+     * $this->attributes['address'] - string - contains the supplier address
+     */
+    protected $fillable = [
+        'name',
+        'email',
+        'address',
+    ];
+
+    protected static array $rules = [
+        'name' => 'required|string|max:255',
+        'email' => 'required|email|max:255|unique:suppliers,email',
+        'address' => 'required|string|max:500',
+    ];
+
+    /*
+     * GETTERS
+    */
+    public function getId(): int
+    {
+        return $this->id;
+    }
+
+    public function getName(): string
+    {
+        return $this->name;
+    }
+
+    public function getEmail(): string
+    {
+        return $this->email;
+    }
+
+    public function getAddress(): string
+    {
+        return $this->address;
+    }
+
+    public function getCreatedAt(): Date
+    {
+        return $this->created_at;
+    }
+
+    public function getUpdatedAt(): Date
+    {
+        return $this->updated_at;
+    }
+
+    /*
+     * SETTERS
+    */
+    public function setName(string $name): void
+    {
+        $this->name = $name;
+    }
+
+    public function setEmail(string $email): void
+    {
+        $this->email = $email;
+    }
+
+    public function setAddress(string $address): void
+    {
+        $this->address = $address;
+    }
+
+    /*
+     * VALIDATE
+    */
+    public static function validate(array $supplierDataValidated): array
+    {
+        return validator($supplierDataValidated, static::$rules)->validate();
+    }
+}
