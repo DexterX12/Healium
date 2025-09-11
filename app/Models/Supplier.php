29a10cda00d37c8126e85ba233c0cbd99ddf7829
stufonn-cell <stufonn@gmail.com>
2025-09-11 13:41:35 -0500

    Delete: Unnecessary library

--- app/Models/Supplier.php
+++ app/Models/Supplier.php
@@ -2,7 +2,6 @@
 
 namespace App\Models;
 
-use Illuminate\Database\Eloquent\Collection;
 use Illuminate\Database\Eloquent\Model;
 use Illuminate\Support\Facades\Date;
 use Illuminate\Database\Eloquent\Relations\HasMany;
