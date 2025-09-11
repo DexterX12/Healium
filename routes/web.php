b420843df15de29e9b8992b7ee398012dfdf1f3e
stufonn-cell <stufonn@gmail.com>
2025-09-11 11:37:35 -0500

    Add: drug/supplier routes

--- routes/web.php
+++ routes/web.php
@@ -2,6 +2,26 @@
 
 use Illuminate\Support\Facades\Route;
 
-Route::get('/', function () {
-    return view('welcome');
-});
+Route::get('/', 'App\Http\Controllers\HomeController@index')
+    ->name('home.index');
+
+Route::get('drug.index', 'App\Http\Controllers\DrugController@index')
+    ->name('drug.index');
+
+Route::get('drug.create', 'App\Http\Controllers\DrugController@create')
+    ->name('drug.create');
+
+Route::get('drug.show/{id}', 'App\Http\Controllers\DrugController@show')
+    ->name('drug.view');
+
+Route::get('supplier.save', 'App\Http\Controllers\SupplierController@save')
+    ->name('supplier.save');
+
+Route::get('supplier.create', 'App\Http\Controllers\SupplierController@create')
+    ->name('supplier.create');
+
+Route::get('supplier.show', 'App\Http\Controllers\SupplierController@show')
+    ->name('supplier.show');
+
+Route::get('supplier.index', 'App\Http\Controllers\SupplierController@index')
+    ->name('supplier.index');
