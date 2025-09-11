a904c8eb4d7b1203c2f2f6e44dae9042b28b0e8e
stufonn-cell <stufonn@gmail.com>
2025-09-11 11:36:40 -0500

    Add: Drug Controller

---
+++ app/Http/Controllers/DrugController.php
@@ -0,0 +1,53 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\Drug;
+use Illuminate\Http\RedirectResponse;
+use Illuminate\Http\Request;
+use Illuminate\Http\View\View;
+
+class DrugController extends Controller
+{
+    public function index(): View
+    {
+        $viewData = [];
+        $viewData['drugs'] = Drug::all();
+
+        return view('drug.index')->with('viewData', $viewData);
+    }
+
+    public function create(): View
+    {
+        return view('drug.create');
+    }
+
+    public function show(int $id): View
+    {
+        $viewData = [];
+        $selectedDrug = Drug::findOrFail($id);
+        $viewData['drug'] = $selectedDrug;
+
+        return view('drug.view')->with('viewData', $viewData);
+    }
+
+    public function save(Request $request): RedirectResponse
+    {
+        $drugDataValidated = $request->validate($request->all());
+        Drug::create($drugDataValidated);
+
+        return redirect()
+            ->route('drug.index')
+            ->with('success', 'Drug created successfully');
+    }
+
+    public function delete(int $id): RedirectResponse
+    {
+        $drug = Drug::findOrFail($id);
+        $drug->delete();
+
+        return redirect()
+            ->route('drug.index')
+            ->with('success', 'Drug deleted successfully');
+    }
+}
