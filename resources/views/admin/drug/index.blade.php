d4b671dc67ce82bc99f71c73215624c21733fb3b
DexterX12 <65998342+DexterX12@users.noreply.github.com>
2025-09-20 21:20:21 -0500

    Feature: add create button to supplier and drug indexes for redirection

--- resources/views/admin/drug/index.blade.php
+++ resources/views/admin/drug/index.blade.php
@@ -1,6 +1,11 @@
 @extends('layouts.admin')
 @section('content')
-<h2 class="text-center">Currently listed drugs</h2>
+<div class="mb-3">
+    <h2 class="text-center">Currently listed drugs</h2>
+    <div class="d-flex justify-content-end mt-2">
+        <a href="{{ route('admin.drug.create') }}" class="btn btn-primary">Add Drug</a>
+    </div>
+</div>
 <div class="list-group">
     @foreach($viewData['drugs'] as $drug)
         <a href="{{ route('admin.drug.show', ['id' => $drug->getId()]) }}" class="list-group-item list-group-item-action">
--- resources/views/admin/supplier/index.blade.php
+++ resources/views/admin/supplier/index.blade.php
@@ -1,6 +1,11 @@
 @extends('layouts.admin')
 @section('content')
-<h2 class="text-center">Currently registered suppliers</h2>
+<div class="mb-3">
+    <h2 class="text-center">Currently registered suppliers</h2>
+    <div class="d-flex justify-content-end mt-2">
+        <a href="{{ route('admin.supplier.create') }}" class="btn btn-primary">Add Supplier</a>
+    </div>
+</div>
 <div class="list-group">
     @foreach($viewData['suppliers'] as $supplier)
         <a href="{{ route('admin.supplier.show', ['id' => $supplier->getId()]) }}" class="list-group-item list-group-item-action">
