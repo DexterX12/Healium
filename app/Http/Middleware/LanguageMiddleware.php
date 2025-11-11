cc32023162e882c5511c0e5ace692940388bce28
DexterX12 <65998342+DexterX12@users.noreply.github.com>
2025-11-10 23:19:30 -0500

    Update: add remaining lang text

--- app/Http/Middleware/LanguageMiddleware.php
+++ app/Http/Middleware/LanguageMiddleware.php
@@ -3,13 +3,14 @@
 namespace App\Http\Middleware;
 
 use Closure;
+use Illuminate\Http\RedirectResponse;
 use Illuminate\Http\Request;
 use Illuminate\Http\Response;
 use Illuminate\Support\Facades\App;
 
 class LanguageMiddleware
 {
-    public function handle(Request $request, Closure $next): Response
+    public function handle(Request $request, Closure $next): Response | RedirectResponse
     {
 
         if (session()->has('app_locale')) {
